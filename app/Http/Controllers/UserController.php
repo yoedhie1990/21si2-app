ceb34d0f50f2fd2292cf7ec1cc22fe49177e289d
Yudhistira <yudhistira.adhitya@gmail.com>
2023-11-30 18:02:21 +0700

    tambah hashing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -26,6 +27,9 @@ class UserController extends Controller
             'email'=> ['unique:users,email'],
             'password'=> 'required'
         ]);
+
+        $data['password'] = Hash::make($data['password']);
+
         User::create($data);
 
         $request->session()->flash('success','Pendaftaran Berhasil');
